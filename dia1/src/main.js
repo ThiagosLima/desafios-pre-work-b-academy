1d84f9da413c51b91a95de285db11fe7bea7c7d6
Thiago Lima <thiagolima.unb@gmail.com>
2021-08-17 15:03:22 -0300

    Add toggle button to show and hide app (Ex04)

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,21 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+const toggle = document.querySelector('[data-js="toggle-app"]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+toggle.addEventListener('click', event => {
+  event.preventDefault()
+
+  if (app.style.display === 'none') {
+    app.style.display = 'block'
+    event.currentTarget.innerHTML = 'Esconder app'
+  } else {
+    app.style.display = 'none'
+    event.currentTarget.innerHTML = 'Mostrar app'
+  }
+})
