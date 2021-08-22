dd2e852a20cae51afb93c2e7cd1cd88acd190c24
Thiago Lima <thiagolima.unb@gmail.com>
2021-08-22 13:40:49 -0300

    Add get cars(Ex02)

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const url = 'http://localhost:3333/cars'
+// const form = document.querySelector('[data-js="cars-form"]')
+const table = document.querySelector('[data-js="cars-table"]')
+
+fetch(url)
+  .then(result => result.json())
+  .then(result => showCars(result))
+
+function showCars(cars) {
+  if (cars.length === 0) {
+    const row = table.insertRow(-1)
+    row.innerHTML = "Nenhum carro encontrado"
+  } else {
+    console.log('Existem carros cadastrados')
+  }
+}
